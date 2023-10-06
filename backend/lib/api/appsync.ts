0b07961831a6be55fe60eba4e560a0365ca78989
focusotter <mtliendo@focusotter.com>
2023-10-05 21:28:24 -0500

    add more resources

--- backend/lib/api/appsync.ts
+++ backend/lib/api/appsync.ts
@@ -2,6 +2,7 @@ import {
 	AmplifyGraphqlApi,
 	AmplifyGraphqlDefinition,
 } from '@aws-amplify/graphql-api-construct'
+import { AuthorizationType } from 'aws-cdk-lib/aws-appsync'
 import { UserPool } from 'aws-cdk-lib/aws-cognito'
 import { Construct } from 'constructs'
 
@@ -9,14 +10,20 @@ type AmplifyGraphQLAPIProps = {
 	userpool: UserPool
 }
 
-const createAmplifyGraphQLAPI = (
+export const createAmplifyGraphQLAPI = (
 	scope: Construct,
 	props: AmplifyGraphQLAPIProps
 ) => {
 	const api = new AmplifyGraphqlApi(scope, 'MicroSaaSAPI', {
 		definition: AmplifyGraphqlDefinition.fromFiles(),
-		authorizationModes: {},
+		authorizationModes: {
+			defaultAuthorizationMode: AuthorizationType.USER_POOL,
+			userPoolConfig: {
+				userPool: props.userpool,
+			},
+		},
 	})
 
+	api.resources.cfnResources.cfnGraphqlApi.xrayEnabled = true
 	return api
 }
---
+++ backend/lib/functions/postConfirmation/contstruct.ts
@@ -0,0 +1,44 @@
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+import path = require('path')
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
+
+type CreateAddUserFuncProps = {
+	appName: string
+	functionName: string
+	userTableArn: string
+	environmentVars: {
+		USER_TABLE_NAME: string
+		STRIPE_SECRET_NAME: string
+	}
+}
+export const createAddUserFunc = (
+	scope: Construct,
+	props: CreateAddUserFuncProps
+) => {
+	const addUserFunc = new NodejsFunction(
+		scope,
+		`${props.appName}-addUserFunc`,
+		{
+			functionName: `${props.appName}-${props.functionName}`,
+			runtime: Runtime.NODEJS_16_X,
+			handler: 'handler',
+			entry: path.join(__dirname, `./main.ts`),
+			environment: props.environmentVars,
+			bundling: {
+				nodeModules: ['stripe'],
+			},
+		}
+	)
+
+	// ALLOW PUT METHOD TO ACCESS USER TABLE
+	addUserFunc.addToRolePolicy(
+		new PolicyStatement({
+			actions: ['dynamodb:PutItem'],
+			resources: [props.userTableArn],
+		})
+	)
+
+	return addUserFunc
+}
